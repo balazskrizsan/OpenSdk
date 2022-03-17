fec233ab7b9cc2f58d195176d188a4a6806982d6
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-03-17 23:34:31 +0100

    CS

--- OpenSdk/src/ValueObjects/Generator/Schema.cs
+++ OpenSdk/src/ValueObjects/Generator/Schema.cs
@@ -5,9 +5,9 @@ namespace OpenSdk.ValueObjects.Generator
 {
     public class Schema
     {
-        [ReadOnly(true)] public string Name { get; }
-        [ReadOnly(true)] public string Type { get; }
-        [ReadOnly(true)] public Dictionary<string, string> Parameters { get; }
+        public string Name { get; }
+        public string Type { get; }
+        public Dictionary<string, string> Parameters { get; }
 
         public Schema(string name, string type, Dictionary<string, string> parameters)
         {
